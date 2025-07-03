ab1e7bbe4671690f8cf68f82dbcae2dfaeac83d7
Rahadmr645 <rahadmr645@gmail.com>
2025-07-03 16:59:44 +0300

    add icons

--- frontend/src/components/JobsView.jsx
+++ frontend/src/components/JobsView.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { FaRegEdit } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
 const JobsView = ({ title, description, company, location, type, time, ownar }) => {
     return (
         <div className='job-view'>
@@ -23,7 +24,10 @@ const JobsView = ({ title, description, company, location, type, time, ownar }) 
              {new Date(time).toLocaleDateString()}
             </p>
             <p>Made By: {ownar}</p>
-
+            <div>
+            <p> <FaRegEdit style={{border:"1px solid red",width:"20px",height:"20px"}} />bsbsbh</p> 
+              <MdDeleteOutline />
+            </div>
         </div>
     )
 }
