a7442b8271bb37370c7f57ad4fcf5d11f4294d42
Rahadmr645 <rahadmr645@gmail.com>
2025-07-10 23:12:09 +0300

    just live

---
+++ frontend/src/components/Spiner.jsx
@@ -0,0 +1,12 @@
+import react from "react"
+
+const Spiner = () => {
+  return (
+    <div class="spinner-border" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
+    )
+}
+
+
+export default Spiner
